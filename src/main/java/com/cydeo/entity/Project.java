e5a078177713c200a17d55372db6a0a55df7efaf
Sumeye <dalkilincsumeye@gmail.com>
2022-11-13 15:47:53 -0500

    More work added

--- src/main/java/com/cydeo/entity/Project.java
+++ src/main/java/com/cydeo/entity/Project.java
@@ -20,13 +20,18 @@ public class Project extends BaseEntity {
 
     private String projectCode;
     private String projectName;
+
     @Column(columnDefinition = "DATE")
     private LocalDate startDate;
+
     @Column(columnDefinition = "DATE")
     private LocalDate endDate;
+
     @Enumerated(EnumType.STRING)
     private Status projectStatus;
+
     private String projectDetail;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "manager_id")
     private User assignedManager;
