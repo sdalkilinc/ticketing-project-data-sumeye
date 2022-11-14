b18c05fe41b5a3085091889d651475f67bcf310c
Sumeye <dalkilincsumeye@gmail.com>
2022-11-14 12:44:52 -0500

    More work added

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,24 +1,29 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
     @Override
     public List<TaskDTO> listAllTasks() {
-        return null;
+
+        return taskRepository.findAll().stream().map(taskMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
